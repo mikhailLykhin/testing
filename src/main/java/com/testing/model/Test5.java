56835102ea86dd82ee8bfbd980ab1d731b96a973
L3v1strauss <L3v1s@mail.ru>
2021-07-04 11:39:46 +0300

    commit add Test5

---
+++ src/main/java/com/testing/model/Test5.java
@@ -0,0 +1,20 @@
+package com.testing.model;
+
+public class Test5 {
+
+    public void doTest() {
+        System.out.println("do test...");
+    }
+
+    public void isTest() {
+        System.out.println("true or false?");
+    }
+
+    public void deleteTest() {
+        System.out.println("Delete this test");
+    }
+
+    public void doCollaborateTest() {
+        System.out.println("Collaborate test");
+    }
+}
