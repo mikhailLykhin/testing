d13d7900ac6183e799ca978dafd556560e7d9b37
L3v1strauss <L3v1s@mail.ru>
2021-07-04 11:18:56 +0300

    commit isTest

--- src/main/java/com/testing/model/Test1.java
+++ src/main/java/com/testing/model/Test1.java
@@ -5,4 +5,8 @@ public class Test1 {
     public void doTest() {
         System.out.println("do test...");
     }
+
+    public void isTest() {
+        System.out.println("true or false?");
+    }
 }
