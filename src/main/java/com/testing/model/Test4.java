4c0d797ca83dea996442a89cf1226cae7261ff56
L3v1strauss <L3v1s@mail.ru>
2021-07-04 11:26:10 +0300

    commit test4 isTest deleteTest

--- src/main/java/com/testing/model/Test4.java
+++ src/main/java/com/testing/model/Test4.java
@@ -5,4 +5,12 @@ public class Test4 {
     public void doTest() {
         System.out.println("do test...");
     }
+
+    public void isTest() {
+        System.out.println("true or false?");
+    }
+
+    public void deleteTest() {
+        System.out.println("Delete this test");
+    }
 }
